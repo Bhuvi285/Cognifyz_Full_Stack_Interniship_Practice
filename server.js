ead1759efcec56caf24eae97ba694f91744892c9
Bhuvanesh <bhuvineve05@gmail.com>
2026-01-07 09:22:00 +0530

    add server side validations

--- server.js
+++ server.js
@@ -17,6 +17,36 @@ app.get("/", (req, res) => {
 app.post("/submit", (req, res) => {
     const { username, email, password, fullname, phone, gender, country, bio, experience, confirmpassword } = req.body;
 
+    //Adding server-side validations
+
+    // Check required fields
+    if(!username || !email || !password || !fullname || !phone ){
+        return res.status(400).send("All fields are required.");
+    }
+
+    //Email format validation
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(!emailPattern.test(email)){
+        return res.status(400).send("Invalid email.");
+    }
+
+    //phone number validation
+    if(!/^[0-9]{10}$/.test(phone)){
+        return res.status(400).send("Invalid phone number.");
+    }
+
+    //password match 
+    if(password !== confirmpassword){
+        return res.status(400).send("password do not match");
+    }
+
+    const validExperience = ["beginner", "intermediate", "expert"];
+    if(!validExperience.includes(experience)){
+        return res.status(400).send("Invalid experience");
+    }
+
+    res.send("Server-side validation passed!");
+
     res.render("result", {
         username, email, password, fullname, phone, gender, country, bio, experience, confirmpassword
     });
